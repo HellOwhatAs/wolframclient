a492b2fc685db492ea2ad44dc7a581e76aaf6a4b
HellOwhatAs <88815487+HellOwhatAs@users.noreply.github.com>
2022-07-11 11:33:14 +0800

    Update test1.py

--- examples/test1.py
+++ examples/test1.py
@@ -1,5 +1,5 @@
-from wwolframclient import section,wplot,wl,wlexpr
+from wwolframclient import session,wplot,wl,wlexpr
 a,b,c,d,e,x=wl.a,wl.b,wl.c,wl.d,wl.e,wl.x
-Solve=section.function(wl.Solve)
+Solve=session.function(wl.Solve)
 print(Solve(a*x**2+b*x+c==0,x))
-section.terminate()
+session.terminate()
